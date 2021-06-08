d56d27ecd7fff0c484f5908b6d2fb9e8bd18af49
nathanlct <nathan.lichtle@gmail.com>
2021-06-08 18:05:10 +0200

    Remove commented code

--- callbacks.py
+++ callbacks.py
@@ -15,68 +15,6 @@ from env.accel_controllers import IDMController, TimeHeadwayFollowerStopper
 from env.energy_models import PFMMidsizeSedan
 
 
-    # def on_episode_end(self, *, worker: RolloutWorker, base_env: BaseEnv,
-    #                    policies: Dict[str, Policy], episode: MultiAgentEpisode,
-    #                    env_index: int, **kwargs):
-    #     env = base_env.get_unwrapped()[0]
-    #
-    #     for controller in ['rl', 'idm']:
-    #         test_env = TrajectoryEnv(config=env.config)
-    #         test_env.whole_trajectory = True
-    #         state = test_env.reset()
-    #         done = False
-    #         total_reward = 0
-    #         total_distance = 0
-    #         total_energy = 0
-    #
-    #         actions = []
-    #         headways = []
-    #         speed_deltas = []
-    #
-    #         if controller == 'idm':
-    #             idm = IDMController(a=env.max_accel, b=env.max_decel)
-    #             test_env.use_fs = False  # for IDM only
-    #
-    #         while not done:
-    #             if controller == 'rl':
-    #                 action = policies['default_policy'].compute_single_action(state, clip_actions=True, explore=False)[0][0]
-    #             elif controller == 'idm':
-    #                 action = idm.get_accel(state[0] * SPEED_SCALE, state[1] * SPEED_SCALE, state[2] * DISTANCE_SCALE)
-    #             state, reward, done, _ = test_env.step(action)
-    #
-    #             actions.append(action)
-    #             headways.append(state[2] * DISTANCE_SCALE)
-    #             speed_deltas.append( np.abs(state[0] - state[1]) * SPEED_SCALE )
-    #
-    #             total_reward += reward
-    #             total_distance += test_env.av['speed']
-    #             total_energy += test_env.energy_model.get_instantaneous_fuel_consumption(test_env.av['last_accel'], test_env.av['speed'], grade=0)
-    #
-    #         episode.custom_metrics[f"{controller}_traj_mpg"] = (total_distance / 1609.34) / (total_energy / 3600 + 1e-6)
-    #         episode.custom_metrics[f"{controller}_traj_reward"] = total_reward
-    #
-    #         episode.custom_metrics[f"{controller}_avg_abs_action"] = np.mean(np.abs(actions))
-    #         episode.custom_metrics[f"{controller}_std_abs_action"] = np.std(np.abs(actions))
-    #         episode.custom_metrics[f"{controller}_min_action"] = np.min(actions)
-    #         episode.custom_metrics[f"{controller}_max_action"] = np.max(actions)
-    #
-    #         episode.custom_metrics[f"{controller}_avg_headway"] = np.mean(headways)
-    #         episode.custom_metrics[f"{controller}_std_headway"] = np.std(headways)
-    #         episode.custom_metrics[f"{controller}_min_headway"] = np.min(headways)
-    #         episode.custom_metrics[f"{controller}_max_headway"] = np.max(headways)
-    #
-    #         episode.custom_metrics[f"{controller}_avg_speed_delta"] = np.mean(speed_deltas)
-    #         episode.custom_metrics[f"{controller}_std_speed_delta"] = np.std(speed_deltas)
-    #         episode.custom_metrics[f"{controller}_min_speed_delta"] = np.min(speed_deltas)
-    #         episode.custom_metrics[f"{controller}_max_speed_delta"] = np.max(speed_deltas)
-
-
-# can also plot vdes
-# and action/reward regularisations
-# and maybe some of them simulatenously on the same plot (eg rwd/rwd regularisations, or lead speed/vdes)
-# can also plot mpg etc on the rollout plots...
-# actually it should be exactly the same data -> make a function that does both
-
 class TensorboardCallback(BaseCallback):
     """Callback for plotting additional metrics in tensorboard."""
     def __init__(self, eval_freq, eval_at_start, eval_at_end):
