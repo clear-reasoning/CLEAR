ecef696e5ea4ebd7e9bd365ff407304e19bf9391
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2021-06-07 21:10:10 -0400

    add setup file

--- train_setup.py
+++ train_setup.py
@@ -41,7 +41,7 @@ def start_training(args):
     env_config = {
         'max_accel': 1.5,
         'max_decel': 3.0,
-        'horizon': 500,
+        'horizon': 800,
         'min_speed': 0,
         'max_speed': 40,
         'max_headway': 80,
