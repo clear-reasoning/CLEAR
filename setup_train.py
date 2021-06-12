9e18d71ce825216484072b78567f7bc131e53dbb
nathanlct <nathan.lichtle@gmail.com>
2021-06-12 19:20:04 +0200

    Create dir before saving env config

--- setup_train.py
+++ setup_train.py
@@ -58,7 +58,9 @@ def start_training(args):
         'num_idm_cars': config['env_num_idm_cars'],
         'num_concat_states': config['env_num_concat_states']
     }
-    with open(os.path.join(os.path.join(exp_logdir, gs_str), 'env_config.json'), 'w') as fp:
+    gs_dir_path = os.path.join(exp_logdir, gs_str)
+    os.makedirs(gs_dir_path, exist_ok=True)
+    with open(os.path.join(gs_dir_path, 'env_config.json'), 'w') as fp:
         print(f'saved env config to {fp.name}')
         json.dump(env_config, fp)
 
