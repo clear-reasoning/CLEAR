c1ed63a23126c3d9e248ee41a1933e633da1802e
nathanlct <nathan.lichtle@gmail.com>
2021-06-12 16:21:27 +0200

    Debug script

---
+++ debug/main.py
@@ -0,0 +1,27 @@
+import gym
+import time
+import multiprocessing
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import BaseCallback
+
+class TimePerIterCallback(BaseCallback):
+    def __init__(self):
+        super().__init__()
+        self.t0 = time.time()
+
+    def _on_rollout_start(self):
+        self.logger.record('time/iter_rollout_duration', time.time() - self.t0)
+        self.t0 = time.time()
+
+    def _on_rollout_end(self):
+        self.logger.record('time/iter_sgd_duration', time.time() - self.t0)
+        self.t0 = time.time()
+
+    def _on_step(self):
+        return True
+
+env = gym.make('CartPole-v1')
+model = PPO('MlpPolicy', env, verbose=1, n_steps=640, n_epochs=100)
+model.learn(total_timesteps=640*3, callback=TimePerIterCallback())
+
