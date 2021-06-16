562ec2dfa42d5add7874ec1f17f2cee5437d7ba9
nathanlct <nathan.lichtle@gmail.com>
2021-06-17 00:11:50 +0200

    Update example commands

--- run_savio.sh
+++ run_savio.sh
@@ -21,3 +21,14 @@ python savio.py "python train.py --logdir /global/scratch/$USER --s3 --expname o
 --n_envs 1 --gae_lambda 0.99 0.95 0.9 --gamma 0.99 0.95 --batch_size 5120 \
 --cp_frequency 100 --augment_vf 1 --hidden_layer_size 64 --network_depth 4 --env_horizon 500 1000 \
 --env_num_concat_states 1 5 10 --n_processes 20 --eval_frequency 100" --jobname oslo --mail nathan.lichtle@gmail.com
+
+# 16/06/2021 - branch modified_reward_func
+python savio.py "python train.py --logdir /global/scratch/$USER --s3 --expname feca --iters 3200 --n_steps 25600 --use_fs 0 \
+--n_envs 1 --gae_lambda 0.99 0.95 0.9 --gamma 0.99 0.95 --lr .0003 .00003 --batch_size 5120 \
+--cp_frequency 100 --augment_vf 1 --hidden_layer_size 64 --network_depth 4 --env_horizon 500 1000 \
+--env_num_concat_states 1 5 --n_processes 20 --eval_frequency 50" --jobname feca --mail nathan.lichtle@gmail.com
+
+python savio.py "python train.py --logdir /global/scratch/$USER --s3 --expname cra --iters 3200 --n_steps 25600 --use_fs 0 \
+--n_envs 1 --gae_lambda 0.99 0.95 0.9 --gamma 0.99 0.95 --batch_size 5120 \
+--cp_frequency 100 --augment_vf 1 --hidden_layer_size 64 --network_depth 4 --env_horizon 500 1000 \
+--env_num_concat_states 1 5 10 --n_processes 20 --eval_frequency 50" --jobname cra --mail nathan.lichtle@gmail.com
