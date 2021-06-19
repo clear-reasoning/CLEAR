a8f1fb4ed58ede323f257e065b96c85be32a1534
nathanlct <nathan.lichtle@gmail.com>
2021-06-19 17:01:31 +0200

    Fix time-space diagram script

--- visualize/tsd.py
+++ visualize/tsd.py
@@ -3,18 +3,38 @@ import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 from matplotlib import colors
 import numpy as np
+import sys
 
-csv_path = './test.csv'
-df = pd.read_csv(csv_path)
-#  time,env_step,trajectory_index,veh_types,veh_positions,veh_speeds
+
+
+print('Reading emissions data')
+
+emissions_path = sys.argv[1]
+df = pd.read_csv(emissions_path)
+#  time,step,id,position,speed,accel,headway,leader_speed,speed_difference,leader_id,follower_id,instant_energy_consumption,total_energy_consumption,total_distance_traveled,total_miles,total_gallons,avg_mpg
 
 start = 0
 end = 10000
 
-timesteps = df['time'][start:end]
-positions = np.array(list(map(lambda s: list(map(float, s.split(';'))), df['veh_positions'])))[start:end]
-speeds = np.array(list(map(lambda s: list(map(float, s.split(';'))), df['veh_speeds'])))[start:end]
-assert (positions.shape == speeds.shape)
+timesteps = sorted(list(set(map(lambda x: round(x, 1), df['time']))))[start:end]
+
+car_types = []
+positions = []
+speeds = []
+
+for ts in timesteps:
+    ts_data = df.loc[df['time'] == ts]
+    car_types.append(list(ts_data['id']))
+    positions.append(list(ts_data['position']))
+    speeds.append(list(ts_data['speed']))
+
+timesteps = np.array(timesteps)
+positions = np.array(positions)
+speeds = np.array(speeds)
+
+print('Done')
+
+
 
 segments = np.array([np.column_stack([timesteps, positions[:,i], speeds[:,i]]) for i in range(positions.shape[1])])
 segments = np.array(np.array_split(segments, (end - start) // 10, axis=1))
